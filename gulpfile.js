cc8e5211d09c1ae60e5eef6be8638a69886b9c9d
Federico Scarpa <fedescarpa@gmail.com>
2017-09-09 15:54:14 -0300

    Improve flexbox basic grid

--- gulpfile.js
+++ gulpfile.js
@@ -6,34 +6,36 @@ const wiredep = require('wiredep');
 
 const $ = glps();
 
-gulp.task('dist', (done) => runs('clear', ['css', 'scss', 'js', 'fonts'], done));
+var out = 'dist';
+
+gulp.task('dist', (done) => runs('clear', ['scss', 'js', 'fonts'], 'css', done));
 
 gulp.task('clear', () => {
-  return del('dist', {force: true});
+  return del(out, {force: true});
 });
 
 gulp.task('css', () => {
   return gulp.src('src/stylesheets/mumuki-styles.scss')
     .pipe($.sass())
     .pipe($.concat('mumuki-styles.css'))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest(`${out}/css`));
 });
 
 gulp.task('scss', ['scss:vendor'], () => {
   return gulp.src('src/stylesheets/**/*.scss')
     .pipe($.replace('@import "../bower_components/', '@import "vendor/'))
-    .pipe(gulp.dest('dist/scss'));
+    .pipe(gulp.dest(`${out}/scss`));
 });
 
 gulp.task('scss:vendor', () => {
   return gulp.src('src/bower_components/**/*.scss')
-    .pipe(gulp.dest('dist/scss/vendor'));
+    .pipe(gulp.dest(`${out}/scss/vendor`));
 });
 
 gulp.task('js', () => {
   return gulp.src(wiredep({devDependencies: true}).js.concat(['src/javascripts/**/*.js']))
     .pipe($.concat('mumuki-styles.js'))
-    .pipe(gulp.dest('dist/javascripts'));
+    .pipe(gulp.dest(`${out}/javascripts`));
 });
 
 gulp.task('fonts', () => {
@@ -42,5 +44,26 @@ gulp.task('fonts', () => {
     'src/bower_components/font-awesome/fonts/**/*',
     'src/bower_components/dev-awesome/dist/fonts/**/*',
   ];
-  return gulp.src(fonts).pipe(gulp.dest('dist/fonts'));
+  return gulp.src(fonts).pipe(gulp.dest(`${out}/fonts`));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/stylesheets/**/*.scss', ['css']);
+});
+
+gulp.task('dev', (done) => {
+  out = 'build';
+  runs('dist', 'watch', 'serve', done);
+});
+
+gulp.task('serve', () => {
+  return gulp.src('.')
+    .pipe($.webserver({
+      host: '0.0.0.0',
+      open: true,
+      port: 4000,
+      livereload: {
+        port: 12345
+      }
+    }));
 });
