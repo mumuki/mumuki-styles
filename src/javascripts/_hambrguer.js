61113940c41f8d6b913c12b4094078b17783c82f
Federico Scarpa <fedescarpa@gmail.com>
2017-11-11 23:50:04 -0300

    improve humburguer menu

--- src/javascripts/_hambrguer.js
+++ src/javascripts/_hambrguer.js
@@ -2,9 +2,8 @@ mumuki.load(function () {
 
   var OPEN_CLASS = 'mu-hamburguer-open';
 
-  $('header .mu-hamburguer').click(function () {
-    $('header .mu-nav').toggleClass(OPEN_CLASS);
-    $('body').toggleClass(OPEN_CLASS);
+  $('header .mu-hamburguer i').click(function () {
+    $('header .mu-nav, body').toggleClass(OPEN_CLASS);
   });
 
 });
