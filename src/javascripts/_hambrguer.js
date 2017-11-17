49776804157757743c377f4c26990f9b707cd8ab
Federico Scarpa <fedescarpa@gmail.com>
2017-11-17 17:59:03 -0300

    fix hamburguer

--- src/javascripts/_hambrguer.js
+++ src/javascripts/_hambrguer.js
@@ -3,7 +3,15 @@ mumuki.load(function () {
   var OPEN_CLASS = 'mu-hamburguer-open';
 
   $('header .mu-hamburguer i').click(function () {
-    $('header .mu-nav, body').toggleClass(OPEN_CLASS);
+
+    var $nav = $('header .mu-nav');
+
+    $('body').toggleClass(OPEN_CLASS);
+    $nav.toggleClass(OPEN_CLASS);
+
+    if ($nav.hasClass(OPEN_CLASS)) {
+      $('.mu-breadcrumb .brand').focus();
+    }
   });
 
 });
