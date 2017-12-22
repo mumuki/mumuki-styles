7cf9cd8079e0ec81d21eeffcf970502e93e57ed7
Federico Scarpa <fedescarpa@gmail.com>
2017-12-22 17:49:35 -0300

    improve responsivness

--- dist/javascripts/mumuki-styles.js
+++ dist/javascripts/mumuki-styles.js
@@ -12773,34 +12773,38 @@ mumuki.load(function () {
   var availableDirections = {
     up: function ($entity, $column) {
       var x1 = $entity.from.position().left + $entity.from.width() + BORDER;
-      var y1 = $column.from.position().top + $column.from.height() / 2;
+      var y1 = $column.from.position().top + $column.from.height() / 2 + 5;
       var x2 = $entity.to.position().left + $entity.to.width() + BORDER;
-      var y2 = $column.to.position().top + $column.to.height() / 2;
+      var y2 = $column.to.position().top + $column.to.height() / 2 + 5;
       return { x1: x1, x2: x2, y1: y1, y2: y2, mi: Math.max(x1, x2) + C * 2, dx1: C, dx2: C };
     },
     down: function ($entity, $column) {
       var x1 = $entity.from.position().left + $entity.from.width() + BORDER;
-      var y1 = $column.from.position().top + $column.from.height() / 2;
+      var y1 = $column.from.position().top + $column.from.height() / 2 + 5;
       var x2 = $entity.to.position().left + $entity.to.width() + BORDER;
-      var y2 = $column.to.position().top + $column.to.height() / 2;
+      var y2 = $column.to.position().top + $column.to.height() / 2 + 5;
       return { x1: x1, x2: x2, y1: y1, y2: y2, mi: Math.max(x1, x2) + C * 2, dx1: C, dx2: C };
     },
     left: function ($entity, $column) {
       var x1 = $entity.from.position().left;
-      var y1 = $column.from.position().top + $column.from.height() / 2;
+      var y1 = $column.from.position().top + $column.from.height() / 2 + 5;
       var x2 = $entity.to.position().left + $entity.to.width() + BORDER;
-      var y2 = $column.to.position().top + $column.to.height() / 2;
+      var y2 = $column.to.position().top + $column.to.height() / 2 + 5;
       return { x1: x1, x2: x2, y1: y1, y2: y2, mi: (x1 + x2) / 2, dx1: -C, dx2: C };
     },
     right: function ($entity, $column) {
       var x1 = $entity.from.position().left + $entity.from.width() + BORDER;
-      var y1 = $column.from.position().top + $column.from.height() / 2;
+      var y1 = $column.from.position().top + $column.from.height() / 2 + 5;
       var x2 = $entity.to.position().left;
-      var y2 = $column.to.position().top + $column.to.height() / 2;
+      var y2 = $column.to.position().top + $column.to.height() / 2 + 5;
       return { x1: x1, x2: x2, y1: y1, y2: y2, mi: (x1 + x2) / 2, dx1: C, dx2: -C };
     },
     up_left: function ($entity, $column) {
-      return;
+      var x1 = $entity.from.position().left;
+      var y1 = $column.from.position().top + $column.from.height() / 2 + 5;
+      var x2 = $entity.to.position().left + $entity.to.width() + BORDER;
+      var y2 = $column.to.position().top + $column.to.height() / 2 + 5;
+      return { x1: x1, x2: x2, y1: y1, y2: y2, mi: (x1 + x2) / 2, dx1: -C, dx2: C };
     },
     up_right: function ($entity, $column) {
       return;
