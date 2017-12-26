2af83497e52914310c3bed61e867f66e124b88dc
Federico Scarpa <fedescarpa@gmail.com>
2017-12-26 16:47:26 -0300

    improve mu-browser styles

--- dist/javascripts/mumuki-styles.js
+++ dist/javascripts/mumuki-styles.js
@@ -12973,141 +12973,58 @@ mumuki.load(function () {
 
 mumuki.load(function () {
 
-  var descriptorType = {
-    file: {
-      open: function (state, filename) {
-        if (state.onFileOpen) {
-          var onFileOpen = eval(state.onFileOpen);
-          if (typeof onFileOpen === 'function') {
-            onFileOpen(filename, lastFolder(state)[filename]);
-          } else {
-            throw new Error('"data-on-file-open" should be a function');
-          }
-        }
-      }
-    },
-    folder: {
-      open: function (state, foldername) {
-        if (state.canBrowse) {
-          var folder = lastFolder(state)[foldername];
-          state.breadcrumb.push({name: foldername, files: folder});
-          createExplorerFrom(state, folder);
-        }
-      }
-    }
-  }
-
-  function back(state) {
-    if (state.breadcrumb.length > 1) {
-      state.breadcrumb.pop();
-      createExplorerFrom(state, lastFolder(state));
-    }
-  }
-
-  function getPath(state) {
-    return state.breadcrumb
-      .map(function (it) {
-        return it.name;
-      })
-      .join('/') || '/';
-  }
-
-  function lastFolder(state) {
-    return state.breadcrumb[state.breadcrumb.length - 1].files;
-  }
-
-  function sortByTypeAndName(obj, key1, key2) {
-    if (typeof obj[key1] === typeof obj[key2]) {
-      return key1.localeCompare(key2);
-    } else if (obj[key1] instanceof Object && obj[key2] instanceof String) {
-      return 1;
-    } else {
-      return -1;
-    }
-  }
-
-  function sortedFilesKeys(files) {
-    return Object.keys(files || {}).sort(function (a, b) {
-      return sortByTypeAndName(files, a, b);
-    });
-  }
-
-  function getListItem(files, key, state) {
-    return $([
-      '<li>',
-      '  <i class="' + fileBrowserClassType(files[key]) + '"></i>',
-      '  <span>' + key + '</span>',
-      '</li>'].join(''))
-    .click(function () {
-      descriptorType[fileBrowserClassType(files[key])].open(state, key);
-    });
-  }
-
-  function fileBrowserClassType(file) {
-    return file instanceof Object ? 'folder' : 'file';
-  }
-
-  function appendFiles($ul, files, state) {
-    sortedFilesKeys(files).forEach(function (key) {
-      $ul.append(getListItem(files, key, state));
-    });
-  }
-
-  function createExplorerFrom(state, files) {
-    state.container.empty();
-    var $ul = $('<ul></ul>');
-    appendFiles($ul, files, state);
-    state.container.append($ul);
-    state.header.find('.mu-file-browser-path').val(getPath(state));
-  }
-
-  function getExplorerHeader(canBrowse) {
+  function getBrowserHeader($browser) {
+    var title = $browser.data('title') || 'Mumuki';
+    var favicon = $browser.data('favicon') || 'https://mumuki.io/logo-alt-png';
+    var url = $browser.data('url') || 'https://mumuki.io';
     return $([
       '<header>',
-          canBrowse ? '<i class="fa fa-fw fa-arrow-left"></i>' : '',
-      '  <input class="mu-file-browser-path" type="text" readonly>',
+      '  <ul class="mu-browser-tabs">',
+      '    <li class="mu-browser-tab active">',
+      '      <img src="', favicon, '">',
+      '      <span>', title, '<span>',
+      '    </li>',
+      '    <li class="mu-browser-tab mu-browser-new-tab">',
+      '      <i class="fa fa-fw fa-plus"></i>',
+      '    </li>',
+      '  </ul>',
+      '  <div class="mu-browser-bar">',
+      '    <i class="mu-arrow-left"></i>',
+      '    <i class="mu-arrow-right"></i>',
+      '    <i class="mu-refresh"></i>',
+      '    <input class="mu-web-browser-path" type="text" readonly value="', url, '">',
+      '  </header>',
       '</header>'
     ].join(''))
   }
 
-  $.fn.renderFileBrowser = function () {
+  function getBrowserMain($browser) {
+    console.log($browser.html());
+    return $([
+      '<main>',
+        $browser.html(),
+      '</main>'
+    ].join(''));
+  }
+
+  $.fn.renderWebBrowser = function () {
     var self = this;
     self.empty();
     self.each(function (i) {
-      var $explorer = $(self[i]);
-      var files = $explorer.data('file');
-      var canBrowse = $explorer.data('can-browse');
-      var onFileOpen = $explorer.data('on-file-open');
+      var $browser = $(self[i]);
 
-      var $header = getExplorerHeader(canBrowse);
-      var $main = $('<main></main>');
+      var $header = getBrowserHeader($browser);
+      var $main = getBrowserMain($browser);
 
-      var ROOT_FOLDER_NAME = '/home/mumuki';
+      $browser.empty();
 
+      $browser.append($header);
+      $browser.append($main);
 
-      if (files instanceof Object) {
-        var state = {
-          container: $main,
-          header: $header,
-          canBrowse: !!canBrowse,
-          onFileOpen: onFileOpen,
-          breadcrumb: [{name: ROOT_FOLDER_NAME, files: files}]
-        };
-
-        $explorer.append($header);
-        $explorer.append($main);
-
-        $header.find('i').click(function () {
-          back(state)
-        });
-
-        createExplorerFrom(state, files);
-
-      }
     });
     return self;
   }
 
-  $('.mu-browser').renderFileBrowser();
+  $('.mu-browser').renderWebBrowser();
 
 });
