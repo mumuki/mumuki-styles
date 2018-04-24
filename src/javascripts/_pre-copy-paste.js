a0c054311eed0a4584e2084712ecbed0eac780ff
Federico Scarpa <fedescarpa@gmail.com>
2018-04-24 17:02:17 -0300

    added mu-pre copy and paste

---
+++ src/javascripts/_pre-copy-paste.js
@@ -0,0 +1,77 @@
+mumuki.load(function () {
+
+  var language = {
+    es: {
+      copy: 'Copiar',
+      copied: 'Copiado'
+    },
+    en: {
+      copy: 'Copy',
+      copied: 'Copied'
+    },
+    pt: {
+      copy: 'Copiar',
+      copied: 'Copiado'
+    },
+  }
+
+  function getCopiedText() {
+    var lang = (mumuki && mumuki.locale) || (navigator.language || navigator.userLanguage).split('-')[0] || 'en';
+    return language[lang];
+  }
+
+  function copyToClipboard(text) {
+    var $temp = $("<textarea>");
+    $("body").append($temp);
+    $temp.val(text).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+
+  function pasteInEditor(text) {
+    var $editor = $('.mu-paste-target');
+    var cursorPos = $editor.prop('selectionStart');
+    var v = $editor.val() || '';
+    var textBefore = v.substring(0,  cursorPos);
+    var textAfter  = v.substring(cursorPos, v.length);
+    $editor.val(textBefore + text + textAfter);
+  }
+
+  function changeTextAndColor($clipboard) {
+    var $span = $clipboard.find('span');
+    $clipboard.addClass('clicked');
+    $span.text(getCopiedText().copied);
+    setTimeout(function () {
+      $span.text(getCopiedText().copy);
+      $clipboard.removeClass('clicked')
+    }, 2.5 * 1000);
+  }
+
+  $.fn.renderCopyPaste = function () {
+    var self = this;
+    self.each(function (i) {
+      var $pre = $(self[i]);
+      var $code = $pre.children('code');
+      if ($code.length > 0) {
+        $pre.children('span').remove();
+        var $clipboard = $('<span>', {
+          class: 'mu-clipboard',
+          html: '<i class="fa fa-fw fa-clipboard"></i> <span>' + getCopiedText().copy + '</span>',
+          click: function () {
+            copyToClipboard($code.text());
+            pasteInEditor($code.text());
+            changeTextAndColor($clipboard);
+          }
+        });
+        $pre.append($clipboard);
+      }
+    });
+
+    return self;
+  }
+
+  mumuki.resize(function () {
+    $('pre').renderCopyPaste();
+  });
+
+});
