f9880e58790da9dbaef1cbcf9b68acdd34825728
Federico Scarpa <fedescarpa@gmail.com>
2018-06-14 17:30:13 -0300

    fixing pre > code selector, needs only pre

--- src/javascripts/_render.js
+++ src/javascripts/_render.js
@@ -2,7 +2,7 @@ mumuki.load(function () {
 
   $.fn.renderMuComponents = function () {
     this.find('.mu-erd').renderERD();
-    this.find('pre > code').renderCopyPaste();
+    this.find('pre').renderCopyPaste();
     this.find('.mu-browser').renderWebBrowser();
     this.find('.mu-sql-table').renderSqlTable();
     this.find('.mu-file-browser').renderFileBrowser();
