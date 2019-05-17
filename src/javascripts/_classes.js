9630a242110162a7dd686cbaf5cc31818a033e75
Federico Scarpa <fedescarpa@gmail.com>
2019-05-17 13:39:32 -0300

    added styling

--- src/javascripts/_classes.js
+++ src/javascripts/_classes.js
@@ -12,12 +12,13 @@ mumuki.load(function () {
     regexMatchAll(/class\s+([A-Z][a-zA-Z0-9_!?$]+).*?\{([\s\S]*?)\}/, code, function (classMatch) {
       const classBody = classMatch[2];
       const aClass = {
+        type: 'class',
         name: '',
         methods: [] ,
         variables: [],
       }
       aClass.name = classMatch[1];
-      regexMatchAll(/var\s+([a-z][a-zA-Z0-9_!?$]+)\s*:\s*([A-Z][a-zA-Z0-9_!?$]+)/, classBody, function (variableMatch) {
+      regexMatchAll(/var\s+([a-z][a-zA-Z0-9_!?$]+)\s*:\s*([a-zA-Z0-9_!?$]+)/, classBody, function (variableMatch) {
         aClass.variables.push({
           name: variableMatch[1],
           type: variableMatch[2],
@@ -42,6 +43,88 @@ mumuki.load(function () {
     return entities;
   }
 
+  function lowercased(string) {
+    return string.toLowerCase().replace(/[_]/g, '-')
+  }
+
+  function entityID(entity, index) {
+    return 'mu-classes-' + index + '-' + lowercased(entity);
+  }
+
+  function variableID(entity, variable, index) {
+    return entityID(entity, index) + '-' + lowercased(variable);
+  }
+
+  function messageID(entity, message, index) {
+    return entityID(entity, index) + '-' + lowercased(message);
+  }
+
+  function paramID(entity, message, param, index, i) {
+    return messageID (entity, message, index) + '-' + i + '-' + lowercased(param);
+  }
+
+  function generateEntityAttributes(entity, index) {
+    let html = '';
+    entity.variables.forEach(function (variable) {
+      html += [
+        '<li id="', variableID(entity.name, variable.name, index), '" class="mu-classes-entity-attribute">',
+        '  <span class="mu-classes-entity-attribute-name">',
+        '    <span class="mu-classes-type">', variable.type, '</span>',
+        '    <span>', variable.name, '</span>',
+        '  </span>',
+        '</li>'
+      ].join('');
+    });
+    return html;
+  }
+
+  function generateParams(entity, message, index) {
+    return message.params.map(function (param, i) {
+      return [
+        '<span id="', paramID(entity.name, message.name, param.name, index, i), '" class="mu-classes-entity-message-param">',
+        '  <span class="mu-classes-type">', param.type, '</span>',
+        '  <span class="mu-classes-param">', param.name, '</span>',
+        '</span>'
+      ].join('');
+    }).join(',');
+  }
+
+  function generateEntityMessages(entity, index) {
+    let html = '';
+    entity.methods.forEach(function (method) {
+      html += [
+        '<li id="', messageID(entity.name, method.name, index), '" class="mu-classes-entity-message">',
+        '  <span class="mu-classes-entity-message-name">',
+        '    <span class="mu-classes-return">', method.return, '</span>',
+        '    <span>',
+               method.name, '(', generateParams(entity, method, index), ')',
+        '    </span>',
+        '  </span>',
+        '</li>'
+      ].join('');
+    });
+    return html;
+  }
+
+  function appendEntities($diagram, entities, index) {
+    entities.forEach(function (entity) {
+      const $entity = $([
+        '<div id="', entityID(entity.name, index), '" class="mu-classes-entity">',
+        '  <div class="mu-classes-entity-name">',
+        '  ', entity.name,
+        '  </div>',
+        entity.variables.length? '  <ul class="mu-classes-entity-attributes">' : '',
+        entity.variables.length?      generateEntityAttributes(entity, index) : '',
+        entity.variables.length? '  </ul>' : '',
+        entity.methods.length?   '  <ul class="mu-classes-entity-messages">' : '',
+        entity.methods.length?        generateEntityMessages(entity, index) : '',
+        entity.methods.length?   '  </ul>' : '',
+        '</div>',
+      ].join(''));
+      $diagram.append($entity);
+    });
+  }
+
   $.fn.renderClasses = function () {
     const self = this;
     self.empty();
@@ -52,7 +135,8 @@ mumuki.load(function () {
       const $diagram = $(self[i]);
 
       const entities = mapEntities($diagram.data('code'));
-      // appendEntities($diagram, entities, i);
+
+      appendEntities($diagram, entities, i);
       // appendConnectors($diagram, entities, i);
     });
     return self;
